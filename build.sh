0b30e3845e64d22b04b4be589ff86e57bf38fd1b
log_alpha <z3981792@126.com>
2020-07-31 01:21:26 +0800

    添加阅读注释

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 set -x
 
 SOURCE_DIR=`pwd`
+# ${param:-default}
+# 表示param不存在时，返回default
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-Debug}
 
+# 创建目录指定编译类型（debug or release），生成工程，进行编译
 mkdir -p $BUILD_DIR/$BUILD_TYPE \
     && cd $BUILD_DIR/$BUILD_TYPE \
     && cmake \
